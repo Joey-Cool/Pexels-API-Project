c5aa1f953ffaf252d411f33330a2c29f06d9cf14
Joey <jahanna06@gmail.com>
2022-08-13 17:32:05 -0400

    added event listener to search form

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
 
+let searchForm = document.querySelector('#search-form');
 
+searchForm.addEventListener('submit', function(e){
+    e.preventDefault();
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       let res = JSON.parse(xhttp.responseText);
-       console.log(res);
-       let photoContainer = document.querySelector('#results');
-       let photoHeader = document.querySelector('#photo-header');
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
 
+            let res = JSON.parse(xhttp.responseText);
+            console.log(res);
+            let photoContainer = document.querySelector('#results');
+            let photoHeader = document.querySelector('#photo-header');
 
-       let photoH2 = document.createElement("h2");
-       photoH2.classList.add('white', 'margin-right');
-       photoH2.innerHTML = "Photos";
+            photoContainer.innerHTML = '';
+            photoHeader.innerHTML = '';
 
-       let photoCount = document.createElement('h2');
-    //    photoCount.classList.add('inline');
-       photoCount.innerHTML = `${res.total_results}`;
+            let photoH2 = document.createElement("h2");
+            photoH2.classList.add('white', 'margin-right');
+            photoH2.innerHTML = "Photos";
 
-       photoHeader.appendChild(photoH2);
-       photoHeader.appendChild(photoCount);
+            let photoCount = document.createElement('h2');
+            photoCount.innerHTML = `${res.total_results}`;
+
+            photoHeader.appendChild(photoH2);
+            photoHeader.appendChild(photoCount);
         
-    //    photoHeader.innerHTML = `<h2><span>Photos</span> ${res.total_results}</h2>`;
-       
-       let photoData = res.photos.map(function(photo){
-        console.log(photo);
-        let photoDiv = document.createElement('div');
-        photoDiv.classList.add('photo-div');
-        photoDiv.innerHTML = `<img src=${photo.src.medium}>
-        
-        `
 
-        photoContainer.appendChild(photoDiv);
-        return photo;
+            let photoData = res.photos.map(function(photo){
+            console.log(photo);
+            let photoDiv = document.createElement('div');
+            photoDiv.classList.add('photo-div');
+            photoDiv.innerHTML = `<img src=${photo.src.medium}>`
 
-        
-       });
+            photoContainer.appendChild(photoDiv);
+            return photo;
 
+            });
     
     }
 };
 
+let queryValue = document.querySelector('#search-bar').value;
 
-xhttp.open("GET", "https://api.pexels.com/v1/search?query=people", true);
+xhttp.open("GET", `https://api.pexels.com/v1/search?query=${queryValue}`, true);
 xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001a5315a2713fd4f69ac1f28dc9bc85768')
 xhttp.send();
+
+});
+
+
+
 
 
 
